f96662fc2aaf23ac4b04a12f1175a6eeed2ef954
Viktor Ingemarsson <viktor.ingemarsson@datavarde.se>
2024-04-27 14:54:27 +0200

    updates for the object detection. Seems to work without parameters

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # %%
+import datetime
 import os
+import threading
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,57 +13,52 @@ TRIGGER_ANIMALS = set(["horse", "elephant", "cow", "bear"])
 N_IMAGES_WITH_TRIGGER_ANIMALS_TO_TRIGGER = 3
 
 
-def predict(model):
-    results = model(["./input_algar/{i}.png" for i in range(N_IMAGES)])
-    total_results = []
-    for result in results:
-        result_animal = 0
-        for box in result.boxes:
-            name_index = int(box.cls[0].item())
-            if result.names[name_index] in TRIGGER_ANIMALS:
-                result_animal = 1
-        total_results.append(result_animal)
-    if total_results > 3:
-        return True
-    return False
-
-
-def delete_old_images():
-    for file in os.listdir("runs/detect/predict"):
-        os.remove(file)
-
-
-def move_possible_moose_files():
-    possible_moose_images = os.listdir("result/")
-    for index, file in enumerate(os.listdir("runs/detect/predict")):
-        shutil.move(file, f"result/{len(possible_moose_images)+index+1}.png")
+def predict():
+    model = YOLO("models/yolov8x.pt")
+    while True:
+        input_images = os.listdir("./input_algar/")
+        if len(input_images) == 0:
+            time.sleep(2)
+            continue
+        input_images = ["input_algar/" + image_name for image_name in input_images]
+        results = model(input_images)
+        for image_index, result in enumerate(results):
+            found_moose = False
+            for box in result.boxes:
+                name_index = int(box.cls[0].item())
+                if result.names[name_index] in TRIGGER_ANIMALS:
+                    new_file_location = input_images[image_index].replace(
+                        "input_algar", "result"
+                    )
+                    shutil.move(input_images[image_index], f"{new_file_location}.png")
+                    found_moose = True
+                    break
+            if not found_moose:
+                os.remove(input_images[image_index])
+        time.sleep(2)
 
 
 def capture_frames(interval, stream_url):
-    model = YOLO("models/yolov8x.pt")
     driver = webdriver.Chrome()
     driver.get(stream_url)
     time.sleep(5)
     accept_button = driver.find_element(By.CLASS_NAME, "c6ead1144")
     driver.execute_script("arguments[0].click();", accept_button)
-    time.sleep(5)
     play_button = driver.find_element(By.CLASS_NAME, "sc-6bf77590-2")
     driver.execute_script("arguments[0].click();", play_button)
-    time.sleep(5)
+    t1 = threading.Thread(target=predict)
+    t1.start()
     while True:
-        for i in range(N_IMAGES):
-            driver.save_screenshot(f"/input_algar/{i}.png")
-            time.sleep(interval)
-        should_email = predict(model=model)
-        if should_email:
-            print("Animal JaoI")
-            move_possible_moose_files()
-        if not should_email:
-            delete_old_images()
+        driver.save_screenshot(
+            f"./input_algar/{str(int(datetime.datetime.now().timestamp()))}.png"
+        )
+        time.sleep(interval)
 
 
 stream_url = (
     "https://www.svtplay.se/video/jN3XL2B/den-stora-algvandringen/idag-00-00?id=jN3XL2B"
 )
-interval = 3  # Interval in seconds
+interval = 5  # Interval in seconds
 capture_frames(interval, stream_url)
+
+# %%
